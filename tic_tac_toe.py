21480895d9c6758aded2b9522ba66fe380e5bf77
gbrough <gbrough@gmail.com>
2021-08-03 13:51:42 -0700

    update computer algorithm

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,7 @@
 import random
-player = 'X'
-computer = 'O'
+
+playerLetter = 'X'
+computerLetter = 'O'
 
 #define the board
 board = [' ' for i in range(10)]
@@ -19,45 +20,37 @@ def is_full():
   return ' ' not in board[1:10]
  
 #check if there is a winner
-def check_win():
-  return (board[1] == board[2] == board[3] == player) or \
-         (board[4] == board[5] == board[6] == player) or \
-         (board[7] == board[8] == board[9] == player) or \
-         (board[1] == board[4] == board[7] == player) or \
-         (board[2] == board[5] == board[8] == player) or \
-         (board[3] == board[6] == board[9] == player) or \
-         (board[1] == board[5] == board[9] == player) or \
-         (board[3] == board[5] == board[7] == player) or \
-         (board[1] == board[2] == board[3] == computer) or \
-         (board[4] == board[5] == board[6] == computer) or \
-         (board[7] == board[8] == board[9] == computer) or \
-         (board[1] == board[4] == board[7] == computer) or \
-         (board[2] == board[5] == board[8] == computer) or \
-         (board[3] == board[6] == board[9] == computer) or \
-         (board[1] == board[5] == board[9] == computer) or \
-         (board[3] == board[5] == board[7] == computer)
+def check_win(board, current_player):
+  return (board[1] == board[2] == board[3] == current_player) or \
+         (board[4] == board[5] == board[6] == current_player) or \
+         (board[7] == board[8] == board[9] == current_player) or \
+         (board[1] == board[4] == board[7] == current_player) or \
+         (board[2] == board[5] == board[8] == current_player) or \
+         (board[3] == board[6] == board[9] == current_player) or \
+         (board[1] == board[5] == board[9] == current_player) or \
+         (board[3] == board[5] == board[7] == current_player)
 
 #is a space free
-def is_free(position):
+def is_free(board, position):
   return board[position] == ' '
 
 #place the marker on the board
-def place_marker(marker, position):
-  board[position] = marker
+def place_marker(board, mark, position):
+  board[position] = mark
 
 #who goes first
 def who_goes_first():
   if random.randint(0, 1) == 0:
-    return player
+    return playerLetter
   else:
-    return computer
+    return computerLetter
 
 #player turn
 def player_turn():
   position = int(input('Choose a position: '))
   try:
-    if is_free(position):
-      place_marker(player, position)
+    if is_free(board, position):
+      place_marker(board, playerLetter , position)
     else:
       print('This position is not free')
       player_turn()
@@ -66,39 +59,56 @@ def player_turn():
       print('Please choose a number between 1 and 9')
       player_turn()
 
+# create a copy of the board
+def duplicate_board(board):
+  duplicate_board = []
+  for i in board:
+    duplicate_board.append(i)
+  return duplicate_board
+
 #computer turn
 def computer_turn():
-  position  = random.randint(1, 9)
-  if is_free(position):
-    place_marker(computer, position)
-  else:
-    computer_turn()
+  #check if computer can win
+  position = random.randint(1, 9)
+  i = 1
+  while i < 10:
+    copy = duplicate_board(board)
+    if is_free(copy, i):
+      if check_win(copy, computerLetter):
+        place_marker(board, computerLetter, position)
+
+  if is_free(board, position):
+    place_marker(board, computerLetter, position)
+      
+      
+
+      
 
 #switch the player and computer
 current_player = who_goes_first()
 def switch_player():
   global current_player
-  if current_player == player:
-    current_player = computer
+  if current_player == playerLetter:
+    current_player = computerLetter
   else:
-    current_player = player
+    current_player = playerLetter
 
 #current player
 def current_player_turn():
-  if current_player == player:
+  if current_player == playerLetter:
     player_turn()
   else:
     computer_turn()
 
-
 #run the game
 def main():
   print('Welcome to Tic Tac Toe!')
+
   while True:
     print_board()
     switch_player()
     current_player_turn()
-    if check_win():
+    if check_win(board, current_player):
       print_board()
       print(current_player + ' won!')
       break
